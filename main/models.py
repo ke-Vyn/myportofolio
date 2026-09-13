b81cac572c3a0e8bef99aece1caaa00e7684694c
ke-Vyn <kevin.ryan51@ui.ac.id>
2026-09-13 15:53:10 +0700

    feat(main): Add project model, migration, and show_projects view

--- main/models.py
+++ main/models.py
@@ -1,7 +1,5 @@
 import uuid
-
 from django.db import models
-
 
 class Experience(models.Model):
     EXPERIENCE_CHOICES = [
@@ -31,3 +29,16 @@ class Experience(models.Model):
     @property
     def is_ongoing(self):
         return self.ended_at is None
+
+class Project(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    tech_stack = models.CharField(
+        max_length=255,
+        help_text="Pisahkan dengan koma (contoh: Python, Django)",
+    )
+    project_url = models.URLField(blank=True, null=True)
+
+    def __str__(self):
+        return self.title
