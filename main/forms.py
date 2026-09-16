eb4938c7a7774908ca50a9ace6fbc072753e0a2f
ke-Vyn <kevin.ryan51@ui.ac.id>
2026-09-16 19:39:12 +0700

    Tutorial 3 - implement base.html template and JSON data delivery

---
+++ main/forms.py
@@ -0,0 +1,46 @@
+from django.forms import ModelForm, TextInput, Textarea, URLInput
+
+from main.models import Project
+
+
+class ProjectForm(ModelForm):
+    class Meta:
+        model = Project
+        fields = [
+            "title",
+            "description",
+            "tech_stack",
+            "project_url",
+        ]
+
+        labels = {
+            "title": "Nama Proyek",
+            "description": "Deskripsi Proyek",
+            "tech_stack": "Teknologi yang Digunakan",
+            "project_url": "URL Proyek",
+        }
+
+        widgets = {
+            "title": TextInput(
+                attrs={
+                    "placeholder": "Portfolio Website",
+                    "maxlength": 255,
+                }
+            ),
+            "description": Textarea(
+                attrs={
+                    "placeholder": "Ceritakan Proyekmu",
+                    "rows": 3,
+                }
+            ),
+            "tech_stack": TextInput(
+                attrs={
+                    "placeholder": "Django, Python, HTML, CSS",
+                }
+            ),
+            "project_url": URLInput(
+                attrs={
+                    "placeholder": "https://github.com/username/repo",
+                }
+            ),
+        }
